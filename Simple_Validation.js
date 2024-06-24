e141acdea2f3b6e517ec24bbd48ec55ceef96e43
Mohamed-AboRahma <aborahma5122005@gmail.com>
2024-06-24 22:57:11 +0300

    set global variable to start project tommorw

---
+++ Simple_Validation.js
@@ -0,0 +1,5 @@
+const subFormContainer = document.querySelector(".subFormContainer");
+const inputEmail = document.querySelector(".inputEmail");
+const inputPassword = document.querySelector(".inputPassword");
+const logIn_Button = document.querySelector(".logIn_Button");
+console.log(subFormContainer, inputEmail, inputPassword, logIn_Button);
